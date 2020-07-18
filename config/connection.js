ac0508e67548404243b01a7677cf1a4b815cbaed
hozerpffp <hozerpffp2009@gmail.com>
2020-07-18 13:58:38 -0500

    Made changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 var mysql = require("mysql");
 var http = require("http");
+var connection;
 // Setting up mysql connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Rootbeer1",
-  database: "eatdaburger_db"
-});
-
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "Rootbeer1",
+    database: 'eatdaburger_db'
+  });
+};
 // Make connection.
 connection.connect((err) => {
   if (err) {                              
